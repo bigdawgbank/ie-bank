de89780c8daeb18560685319b28501923a8bded7
R3call <thenoobyzombie@gmail.com>
2024-10-08 18:12:04 +0200

    feat: included new homepage

--- frontend/src/components/Home.vue
+++ frontend/src/components/Home.vue
@@ -1,10 +1,15 @@
 <template>
-  <div class="jumbotron vertical-center">
-    <div class="container">
-      <div class="row">
-        <div class="col-sm-12">Hello this is home</div>
+  <div class="container-fluid min-vh-100 py-5">
+    <div class="row h-100">
+      <div class="col-12 text-center my-auto">
+        <h1>
+          Welcome to the IE Bank Management
+          <span class="text-primary">System.</span>
+        </h1>
+        <a role="button" href="/accounts" class="btn btn-primary mt-3">
+          Go to accounts →
+        </a>
       </div>
     </div>
   </div>
 </template>
-
