a4f6c7ad784b351c09aaf3a67f3474ffb571c30b
R3call <thenoobyzombie@gmail.com>
2024-11-23 16:46:01 +0100

    chore: removed unused build

--- backend/config.py
+++ backend/config.py
@@ -15,23 +15,7 @@ class LocalConfig(Config):
     DEBUG = True
 
 
-class GithubCIConfig(Config):
-    SQLALCHEMY_DATABASE_URI = "sqlite:///test.db"
-    DEBUG = True
-
-
 class DevelopmentConfig(Config):
-    SQLALCHEMY_DATABASE_URI = "postgresql://{dbuser}:{dbpass}@{dbhost}/{dbname}".format(
-        dbuser=os.getenv("DBUSER"),
-        dbpass=os.getenv("DBPASS"),
-        dbhost=os.getenv("DBHOST"),
-        dbname=os.getenv("DBNAME"),
-    )
-    DEBUG = True
-
-
-# Added custom config
-class UATConfig(Config):
     SQLALCHEMY_DATABASE_URI = "postgresql://{dbuser}:{dbpass}@{dbhost}/{dbname}".format(
         dbuser=os.getenv("DBUSER"),
         dbpass=os.getenv("DBPASS"),
