e502c3da2f3c18d94c68feb5103faea5fbe4a973
R3call <thenoobyzombie@gmail.com>
2024-11-24 16:07:16 +0100

    feat: added new auth routes and route guard

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -2,6 +2,9 @@ import Vue from "vue";
 import VueRouter from "vue-router";
 import AppAccounts from "../components/AppAccounts.vue";
 import Home from "../components/Home.vue";
+import Register from "../components/Register.vue";
+import Login from "../components/Login.vue";
+import { authService } from "@/api";
 
 Vue.use(VueRouter);
 
@@ -12,9 +15,20 @@ const routes = [
     component: Home,
   },
   {
+    path: "/register",
+    name: "Register",
+    component: Register,
+  },
+  {
+    path: "/login",
+    name: "Login",
+    component: Login,
+  },
+  {
     path: "/accounts",
     name: "AppAccounts",
     component: AppAccounts,
+    meta: { requiresAuth: true },
   },
 ];
 
@@ -22,6 +36,27 @@ const router = new VueRouter({
   mode: "history",
   base: process.env.BASE_URL,
   routes,
+});
+
+// Route guard from Flask API
+router.beforeEach(async (to, from, next) => {
+  // Check if the route requires authentication
+  if (to.matched.some((record) => record.meta.requiresAuth)) {
+    // Make a simple request to any protected endpoint
+    try {
+      // This will succeed if we have a valid session cookie
+      const response = await authService.checkSession();
+      if (response.authenticated) {
+        next();
+      } else {
+        next("/login");
+      }
+    } catch (error) {
+      next("/login"); // Error occurred, redirect to login
+    }
+  } else {
+    next(); // Not a protected route, proceed
+  }
 });
 
 export default router;
