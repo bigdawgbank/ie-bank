f17aca85ad5a43f1b72881e78b686c05e02e09a0
ADRIANDLT <adrian.delatorre@outlook.com>
2024-11-25 22:33:48 +0100

    made landing page look better

--- frontend/src/components/Home.vue
+++ frontend/src/components/Home.vue
@@ -1,15 +1,49 @@
 <template>
-  <div class="container-fluid min-vh-100 py-5">
-    <div class="row h-100">
-      <div class="col-12 text-center my-auto">
-        <h1>
-          Welcome to the IE Bank Management
-          <span class="text-primary">System.</span>
-        </h1>
-        <a role="button" href="/accounts" class="btn btn-primary mt-3">
-          Go to accounts →
-        </a>
+  <div class="jumbotron vertical-center">
+    <div class="container">
+      <div class="row">
+        <div class="col-sm-12">
+          <h1>Welcome to the IE Bank Management</h1>
+          <hr />
+          <br />
+
+          <button
+            type="button"
+            class="btn btn-success btn-sm"
+            @click="navigateToAccounts"
+          >
+            Manage Bank Accounts
+          </button>
+          <br /><br />
+
+          <!-- Add the large photo below the button -->
+          <img src="../assets/bank_background_photo.png" alt="Banking photo" class="large-photo" />
+
+          <footer class="text-center">
+            Copyright &copy; All Rights Reserved.
+          </footer>
+        </div>
       </div>
     </div>
   </div>
 </template>
+
+<style>
+.large-photo {
+width: 100%;
+height: auto;
+max-width: 1024px; /* Adjust the max-width as needed */
+display: block;
+margin: 0% left; /* Center the image */
+}
+</style>
+
+<script>
+export default {
+methods: {
+  navigateToAccounts() {
+    this.$router.push('/accounts');
+  }
+}
+}
+</script>
