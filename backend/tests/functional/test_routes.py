fcff2f4bfa2d596bf6b2dc646762b31add774f22
ADRIANDLT <adrian.delatorre@outlook.com>
2024-11-25 22:20:00 +0100

    implemented tests from before

--- backend/tests/functional/test_routes.py
+++ backend/tests/functional/test_routes.py
@@ -1,3 +1,7 @@
+import pytest
+from iebank_api import app
+import json
+
 def test_create_account(testing_client):
     # Register and login
     testing_client.post(
@@ -78,3 +82,72 @@ def test_create_account_unauthorized(testing_client):
         "/accounts", json={"name": "My Account", "currency": "€", "country": "Ireland"}
     )
     assert response.status_code == 401
+
+def test_get_accounts(testing_client):
+    """
+    GIVEN a Flask application
+    WHEN the '/accounts' page is requested (GET)
+    THEN check the response is valid
+    """
+    response = testing_client.get("/accounts")
+    assert response.status_code == 200
+
+def test_dummy_wrong_path():
+    """
+    GIVEN a Flask application
+    WHEN the '/wrong_path' page is requested (GET)
+    THEN check the response is valid
+    """
+    with app.test_client() as client:
+        response = client.get("/wrong_path")
+        assert response.status_code == 404
+
+def test_get_account_by_account_id(testing_client):
+    """
+    GIVEN a Flask application
+    WHEN the '/accounts' page is requested (GET)
+    THEN check the response is valid
+    """
+    # Query all existing accounts
+    response_all = testing_client.get('/accounts')
+    assert response_all.status_code == 200
+
+    # Parse the JSON response
+    accounts = json.loads(response_all.data)
+    
+    # Ensure there are accounts in the response
+    assert len(accounts) > 0
+
+    # Get the first account
+    first_account = accounts['accounts'][0]
+    
+    # Perform assertions on the first account
+    assert 'id' in first_account
+    assert 'account_number' in first_account
+    assert 'name' in first_account
+    assert 'currency' in first_account
+    assert 'country' in first_account
+
+    # Now we can test get by account number
+    response_single = testing_client.get('/accounts/' + str(first_account['id']))
+    assert response_single.status_code == 200
+
+# test update/put to the by default account id=1
+def test_put_account(testing_client):
+    """
+    GIVEN a Flask application
+    WHEN the '/accounts' page is updated (PUT)
+    THEN check the response is valid
+    """
+    response = testing_client.put('/accounts/1', json={'name': 'John Doe', 'currency': '€', 'country': 'Spain'})
+    assert response.status_code == 200
+
+# test_delete_account would delete the by default created account with id=1
+def test_delete_account(testing_client):
+    """
+    GIVEN a Flask application
+    WHEN the '/accounts' page is deleted (DELETE)
+    THEN check the response is valid
+    """
+    response = testing_client.delete('/accounts/1')
+    assert response.status_code == 200
